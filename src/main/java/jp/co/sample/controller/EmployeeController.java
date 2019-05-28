adb9734f5f7b5a0c4367bf6f173d5dff3f016123
yuki.maekawa <yuki.maekawa@rakus.co.jp>
2019-05-28 17:58:10 +0900

    add

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 /**
@@ -23,6 +25,11 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService employeeService;
 	
+	@ModelAttribute
+	public UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+	
 	/**
 	 * 従業員一覧を出力するメソッド.
 	 * 
@@ -34,6 +41,14 @@ public class EmployeeController {
 		List<Employee> employeeList = employeeService.showList();
 		model.addAttribute("employeeList", employeeList);
 		return "employee/list";
+	}
+	
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		Employee employee = employeeService.showDetail(Integer.parseInt(id));
+		model.addAttribute("employee", employee);
+		System.out.println(employee);
+		return "forward:/employee/detail.html";
 	}
 
 }
--- src/main/java/jp/co/sample/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -60,7 +60,7 @@ public class EmployeeRepository {
 	 * @return employee
 	 */
 	public Employee load(Integer id) {
-		String sql = "select id,name,image,gender,hire_date,mail_address,telephone,salary,characteristics,dependents_count "
+		String sql = "select id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,characteristics,dependents_count "
 				    + "from employees where id=:id;";
 		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
 		Employee employee = template.queryForObject(sql, param, EMPLOYEE_ROW_MAPPER);
