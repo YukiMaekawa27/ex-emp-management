39ef24230f01f56120376434ae236c055b41a2da
yuki.maekawa <yuki.maekawa@rakus.co.jp>
2019-05-28 12:28:38 +0900

    Administratorを追加できるようにControllerとServiceを作成しました

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,13 +1,19 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
 
+/**
+ * @author yuki
+ *
+ */
 @Controller
 @RequestMapping("/administrator")
 public class AdministratorController {
@@ -20,9 +26,27 @@ public class AdministratorController {
 		return new InsertAdministratorForm();
 	}
 	
+	/**
+	 * @return 最初の入力画面を表示させる
+	 */
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "administrator/insert";
+	}
+	
+	
+	/**
+	 * 入力された情報をデータベースへ挿入する
+	 * 
+	 * @param form
+	 * @return /へリダイレクト処理を行う
+	 */
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator administrator = new Administrator();
+		BeanUtils.copyProperties(form, administrator);
+		service.insert(administrator);
+		return "redirect:/";
 	}
 
 }
--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -7,6 +7,10 @@ import org.springframework.transaction.annotation.Transactional;
 import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
+/**
+ * @author yuki
+ *
+ */
 @Service
 @Transactional
 public class AdministratorService {
@@ -14,10 +18,20 @@ public class AdministratorService {
 	@Autowired
 	private AdministratorRepository repository;
 	
+	/**
+	 * データベースに情報を登録
+	 * @param administrator
+	 */
 	public void insert(Administrator administrator) {
 		repository.insert(administrator);
 	}
 	
+	/**
+	 * 受け取ったmailAddress、passwordからadministratorを検索する
+	 * @param mailAddress
+	 * @param password
+	 * @return administrator
+	 */
 	public Administrator findByMailAddressAndPassword (String mailAddress, String password) {
 		Administrator administrator = repository.findByMailAddressAndPassword(mailAddress, password);
 		return administrator;
